a61ee08fe60cdca2f67c8cab06f912fc45e1f702
Pashkivska117 <kruglova_katerina>
2024-01-15 20:02:26 +0200

    done ex-6

--- main.py
+++ main.py
@@ -2,11 +2,27 @@ x = 4
 y = 4
 
 i = j = 0
-
+wall = x
 while i < x:
     j = 0
     while j < y:
-        print(f'[{i},{j}]', end=' ')
+        if i >= j:
+            print(f'[{i},{j}]', end=' ')
+        else:
+            print(f'    ', end=' ')
         j += 1
     print()
     i += 1
+
+
+    i = j = 0
+    while i < x:
+        j = 0
+        while j < y:
+            if j <= wall-i-1:
+                print(f'[{i},{j}]', end=' ')
+            else:
+                print(f'     ', end=' ')
+            j += 1
+        print()
+        i += 1
